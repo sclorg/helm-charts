c08a589aa89b9e7963bf847120d3359e9161c190
Petr "Stone" Hracek <phracek@redhat.com>
2023-04-24 12:23:56 +0200

    MariaDB persistent Helm Chart
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

---
+++ charts/redhat/mariadb-persistent/src/README.md
@@ -0,0 +1,7 @@
+# MariaDB helm chart
+
+This repository contains helm chart for MariaDB image build and deployed on OpenShift.
+
+For more information about helm charts see the offical [Helm Charts Documentation](https://helm.sh/).
+
+You need to have access to a cluster for each operation with OpenShift 4, like deploying and testing.
---
+++ tests/test_mariadb_template.py
@@ -0,0 +1,31 @@
+import os
+
+import pytest
+from pathlib import Path
+
+from container_ci_suite.helm import HelmChartsAPI
+
+test_dir = Path(os.path.abspath(os.path.dirname(__file__)))
+
+
+class TestHelmPostgresqlPersistent:
+
+    def setup_method(self):
+        package_name = "mariadb-persistent"
+        path = test_dir / "../charts/redhat"
+        self.hc_api = HelmChartsAPI(path=path, package_name=package_name, tarball_dir=test_dir)
+
+    def teardown_method(self):
+        self.hc_api.delete_project()
+
+    def test_package_persistent(self):
+        self.hc_api.set_version("0.0.1")
+        self.hc_api.package_name = "mariadb-imagestreams"
+        self.hc_api.helm_package()
+        assert self.hc_api.helm_installation()
+        self.hc_api.set_version("0.0.1")
+        self.hc_api.package_name = "mariadb-persistent"
+        self.hc_api.helm_package()
+        assert self.hc_api.helm_installation(values={".image.tag": "13-el8", ".namespace": self.hc_api.namespace})
+        assert self.hc_api.is_pod_running()
+        assert self.hc_api.test_helm_chart(expected_str="^42")

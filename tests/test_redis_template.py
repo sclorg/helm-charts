9bf55ffe9b1131c07471d63dbb07e8367a0e4eb8
Petr "Stone" Hracek <phracek@redhat.com>
2023-04-25 15:37:00 +0200

    Add redis template Helm Chart
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- tests/test_redis_template.py
+++ tests/test_redis_template.py
@@ -8,12 +8,12 @@ from container_ci_suite.helm import HelmChartsAPI
 test_dir = Path(os.path.abspath(os.path.dirname(__file__)))
 
 
-class TestHelmRedisDBPersistent:
+class TestHelmRedisPersistent:
 
     def setup_method(self):
         package_name = "redis-persistent"
         path = test_dir / "../charts/redhat"
-        self.hc_api = HelmChartsAPI(path=path, package_name=package_name, namespace="redis-helm", tarball_dir=test_dir, delete_prj=False)
+        self.hc_api = HelmChartsAPI(path=path, package_name=package_name, tarball_dir=test_dir)
 
     def teardown_method(self):
         self.hc_api.delete_project()
@@ -26,6 +26,6 @@ class TestHelmRedisDBPersistent:
         self.hc_api.set_version("0.0.1")
         self.hc_api.package_name = "redis-persistent"
         self.hc_api.helm_package()
-        assert self.hc_api.helm_installation(values={".redis_version": "8-el8", ".namespace": self.hc_api.namespace})
+        assert self.hc_api.helm_installation(values={".redis_version": "6-el8", ".namespace": self.hc_api.namespace})
         assert self.hc_api.is_pod_running()
         assert self.hc_api.test_helm_chart(expected_str=["PONG"])

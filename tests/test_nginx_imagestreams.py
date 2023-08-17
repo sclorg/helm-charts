aa8a49ab15a4f486a6e6dd9d02828ca84f4643a1
Adam Dobes <adobes@redhat.com>
2023-08-17 10:32:02 +0200

    Add tests for centos nginx-imagestreams

--- tests/test_nginx_imagestreams.py
+++ tests/test_nginx_imagestreams.py
@@ -8,7 +8,7 @@ from container_ci_suite.helm import HelmChartsAPI
 test_dir = Path(os.path.abspath(os.path.dirname(__file__)))
 
 
-class TestHelmMariadbImageStreams:
+class TestHelmRHELNginxImageStreams:
 
     def setup_method(self):
         package_name = "nginx-imagestreams"
@@ -24,6 +24,31 @@ class TestHelmMariadbImageStreams:
             ("1.20-ubi9", "registry.redhat.io/ubi9/nginx-120:latest"),
             ("1.20-ubi8", "registry.redhat.io/ubi8/nginx-120:latest"),
             ("1.20-ubi7", "registry.redhat.io/ubi7/nginx-120:latest"),
+        ],
+    )
+    def test_package_imagestream(self, version, registry):
+        self.hc_api.set_version("0.0.1")
+        self.hc_api.helm_package()
+        self.hc_api.helm_installation()
+        assert self.hc_api.check_imagestreams(version=version, registry=registry)
+
+
+class TestHelmCentOSNginxImageStreams:
+
+    def setup_method(self):
+        package_name = "nginx-imagestreams"
+        path = test_dir / "../charts/centos"
+        self.hc_api = HelmChartsAPI(path=path, package_name=package_name, tarball_dir=test_dir)
+
+    def teardown_method(self):
+        self.hc_api.delete_project()
+
+    @pytest.mark.parametrize(
+        "version,registry",
+        [
+            ("1.20-ubi9", "registry.access.redhat.com/ubi9/nginx-120:latest"),
+            ("1.20-ubi8", "registry.access.redhat.com/ubi8/nginx-120:latest"),
+            ("1.20-ubi7", "registry.access.redhat.com/ubi7/nginx-120:latest")
         ],
     )
     def test_package_imagestream(self, version, registry):

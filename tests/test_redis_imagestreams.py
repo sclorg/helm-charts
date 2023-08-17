fef33b3c2a079ee9f8bd939f79b7fbe2d35e9738
Adam Dobes <adobes@redhat.com>
2023-08-17 10:34:18 +0200

    Add tests for centos redis-imagestreams

--- tests/test_redis_imagestreams.py
+++ tests/test_redis_imagestreams.py
@@ -8,7 +8,7 @@ from container_ci_suite.helm import HelmChartsAPI
 test_dir = Path(os.path.abspath(os.path.dirname(__file__)))
 
 
-class TestHelmRedisImageStreams:
+class TestHelmRHELRedisImageStreams:
 
     def setup_method(self):
         package_name = "redis-imagestreams"
@@ -24,6 +24,29 @@ class TestHelmRedisImageStreams:
             ("6-el9", "registry.redhat.io/rhel9/redis-6:latest"),
             ("6-el7", "registry.redhat.io/rhscl/redis-6-rhel7:latest"),
             ("6-el8", "registry.redhat.io/rhel8/redis-6:latest"),
+        ],
+    )
+    def test_package_imagestream(self, version, registry):
+        self.hc_api.set_version("0.0.1")
+        self.hc_api.helm_package()
+        self.hc_api.helm_installation()
+        assert self.hc_api.check_imagestreams(version=version, registry=registry)
+
+
+class TestHelmCentOSRedisImageStreams:
+
+    def setup_method(self):
+        package_name = "redis-imagestreams"
+        path = test_dir / "../charts/centos"
+        self.hc_api = HelmChartsAPI(path=path, package_name=package_name, tarball_dir=test_dir)
+
+    def teardown_method(self):
+        self.hc_api.delete_project()
+
+    @pytest.mark.parametrize(
+        "version,registry",
+        [
+            ("6-el7", "quay.io/centos7/redis-6-centos7:latest"),
         ],
     )
     def test_package_imagestream(self, version, registry):

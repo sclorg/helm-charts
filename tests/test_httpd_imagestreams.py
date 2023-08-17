7059e7faab0dbe94632bd6659d5aab6334895200
Adam Dobes <adobes@redhat.com>
2023-08-17 09:56:43 +0200

    Add tests for centos httpd-imagestreams

--- tests/test_httpd_imagestreams.py
+++ tests/test_httpd_imagestreams.py
@@ -8,7 +8,7 @@ from container_ci_suite.helm import HelmChartsAPI
 test_dir = Path(os.path.abspath(os.path.dirname(__file__)))
 
 
-class TestHelmHttpdImageStreams:
+class TestHelmRHELHttpdImageStreams:
 
     def setup_method(self):
         package_name = "httpd-imagestreams"
@@ -33,3 +33,29 @@ class TestHelmHttpdImageStreams:
         self.hc_api.helm_package()
         self.hc_api.helm_installation()
         assert self.hc_api.check_imagestreams(version=version, registry=registry)
+
+
+class TestHelmCentOSHttpdImagestreams:
+    def setup_method(self):
+        package_name = "httpd-imagestreams"
+        path = test_dir / "../charts/centos"
+        self.hc_api = HelmChartsAPI(path=path, package_name=package_name, tarball_dir=test_dir)
+    
+    def teardown_method(self):
+        self.hc_api.delete_project()
+
+    @pytest.mark.parametrize(
+        "version,registry", 
+        [
+            ("2.4-ubi9", "registry.access.redhat.com/ubi9/httpd-24:latest"),
+            ("2.4-ubi8", "registry.access.redhat.com/ubi8/httpd-24:latest"),
+            ("2.4-el8", "docker.io/centos/httpd-24-centos8:latest"),
+            ("2.4-el7", "quay.io/centos7/httpd-24-centos7:latest"),
+            ("2.4", "quay.io/centos7/httpd-24-centos7:latest"),
+        ]
+    )
+    def test_package_imagestream(self, version, registry):
+        self.hc_api.set_version("0.0.1")
+        self.hc_api.helm_package()
+        self.hc_api.helm_installation()
+        self.hc_api.check_imagestreams(version=version, registry=registry)

122c6ba97fa0489995c04a7dfd568bd3bad59714
Petr "Stone" Hracek <phracek@redhat.com>
2023-12-12 15:59:28 +0100

    Add nginx-template
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

---
+++ tests/test_nginx_template.py
@@ -0,0 +1,54 @@
+import os
+
+import pytest
+from pathlib import Path
+
+from container_ci_suite.helm import HelmChartsAPI
+
+test_dir = Path(os.path.abspath(os.path.dirname(__file__)))
+
+
+class TestHelmNginxTemplate:
+
+    def setup_method(self):
+        package_name = "nginx-template"
+        path = test_dir / "../charts/redhat"
+        self.hc_api = HelmChartsAPI(path=path, package_name=package_name, tarball_dir=test_dir)
+
+    def teardown_method(self):
+        self.hc_api.delete_project()
+
+    def test_curl_connection(self):
+        self.hc_api.package_name = "nginx-imagestreams"
+        self.hc_api.helm_package()
+        assert self.hc_api.helm_installation()
+        self.hc_api.package_name = "nginx-template"
+        assert self.hc_api.helm_package()
+        assert self.hc_api.helm_installation(
+            values={
+                "nginx_version": "1.20-ubi8",
+                "namespace": self.hc_api.namespace
+            }
+        )
+        expected_str = "Welcome to your static nginx application on OpenShift"
+        assert self.hc_api.is_s2i_pod_running()
+        assert self.hc_api.test_helm_curl_output(
+            route_name="nginx",
+            expected_str=expected_str
+        )
+
+    def test_helm_connection(self):
+        self.hc_api.package_name = "nginx-imagestreams"
+        self.hc_api.helm_package()
+        assert self.hc_api.helm_installation()
+        self.hc_api.package_name = "nginx-template"
+        assert self.hc_api.helm_package()
+        assert self.hc_api.helm_installation(
+            values={
+                "nginx_version": "1.20-ubi8",
+                "namespace": self.hc_api.namespace
+            }
+        )
+        expected_str = "Welcome to your static nginx application on OpenShift"
+        assert self.hc_api.is_s2i_pod_running()
+        assert self.hc_api.test_helm_chart(expected_str=[expected_str])

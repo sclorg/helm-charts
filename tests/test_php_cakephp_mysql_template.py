e8290a5092fe053987cbe2874ea7691d2c11d530
Petr "Stone" Hracek <phracek@redhat.com>
2024-01-16 17:46:42 +0100

    Add php-cakephp-mysql-persistent helm chart
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

---
+++ tests/test_php_cakephp_mysql_template.py
@@ -0,0 +1,58 @@
+import os
+
+import pytest
+from pathlib import Path
+
+from container_ci_suite.helm import HelmChartsAPI
+
+test_dir = Path(os.path.abspath(os.path.dirname(__file__)))
+
+
+class TestHelmCakePHPMySQLTemplate:
+
+    def setup_method(self):
+        package_name = "php-cakephp-mysql-persistent"
+        path = test_dir / "../charts/redhat"
+        self.hc_api = HelmChartsAPI(path=path, package_name=package_name, tarball_dir=test_dir)
+
+    def teardown_method(self):
+        self.hc_api.delete_project()
+
+    def test_curl_connection(self):
+        self.hc_api.package_name = "php-imagestreams"
+        self.hc_api.helm_package()
+        assert self.hc_api.helm_installation()
+        self.hc_api.package_name = "mysql-imagestreams"
+        self.hc_api.helm_package()
+        assert self.hc_api.helm_installation()
+        self.hc_api.package_name = "php-cakephp-mysql-persistent"
+        assert self.hc_api.helm_package()
+        assert self.hc_api.helm_installation(
+            values={
+                "nginx_version": "8.0-ubi8",
+                "namespace": self.hc_api.namespace
+            }
+        )
+        assert self.hc_api.is_s2i_pod_running()
+        assert self.hc_api.test_helm_curl_output(
+            route_name="cakephp-mysql",
+            expected_str="Welcome to CakePHP 4.5"
+        )
+
+    def test_by_helm_test(self):
+        self.hc_api.package_name = "php-imagestreams"
+        self.hc_api.helm_package()
+        assert self.hc_api.helm_installation()
+        self.hc_api.package_name = "mysql-imagestreams"
+        self.hc_api.helm_package()
+        assert self.hc_api.helm_installation()
+        self.hc_api.package_name = "php-cakephp-mysql-persistent"
+        assert self.hc_api.helm_package()
+        assert self.hc_api.helm_installation(
+            values={
+                "nginx_version": "8.0-ubi8",
+                "namespace": self.hc_api.namespace
+            }
+        )
+        assert self.hc_api.is_s2i_pod_running()
+        assert self.hc_api.test_helm_chart(expected_str=["Welcome to CakePHP 4.5"])

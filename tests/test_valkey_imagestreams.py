25351e4eed1710d2416d90d00d5b1c2401197ea8
Petr "Stone" Hracek <phracek@redhat.com>
2024-09-05 09:35:05 +0200

    Add support for CentOS Stream 10 valkey imagestreams
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

---
+++ tests/test_valkey_imagestreams.py
@@ -0,0 +1,29 @@
+import os
+
+import pytest
+from pathlib import Path
+
+from container_ci_suite.helm import HelmChartsAPI
+
+test_dir = Path(os.path.abspath(os.path.dirname(__file__)))
+
+class TestHelmCentOSValkeyImageStreams:
+
+    def setup_method(self):
+        package_name = "valkey-imagestreams"
+        path = test_dir / "../charts/centos"
+        self.hc_api = HelmChartsAPI(path=path, package_name=package_name, tarball_dir=test_dir)
+
+    def teardown_method(self):
+        self.hc_api.delete_project()
+
+    @pytest.mark.parametrize(
+        "version,registry,expected",
+        [
+            ("7-el10", "quay.io/sclorg/valkey-7-c10s:latest", True),
+        ],
+    )
+    def test_package_imagestream(self, version, registry, expected):
+        assert self.hc_api.helm_package()
+        assert self.hc_api.helm_installation()
+        assert self.hc_api.check_imagestreams(version=version, registry=registry) == expected

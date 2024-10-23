abd1cfcb26747363071eac31f984c1df0daf21b9
Petr "Stone" Hracek <phracek@redhat.com>
2024-10-23 19:29:28 +0200

    Do not build and install packages by each imagestream. Once it is enough. We need only to check if imagestream is present.
    Not need to have a lot of projects in OpenShift to created.
    
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- tests/test_php_imagestreams.py
+++ tests/test_php_imagestreams.py
@@ -14,25 +14,25 @@ class TestHelmRHELPHPImageStreams:
         package_name = "php-imagestreams"
         path = test_dir / "../charts/redhat"
         self.hc_api = HelmChartsAPI(path=path, package_name=package_name, tarball_dir=test_dir)
+        assert self.hc_api.helm_package()
+        assert self.hc_api.helm_installation()
 
     def teardown_method(self):
         self.hc_api.delete_project()
 
     @pytest.mark.parametrize(
-        "version,registry",
+        "version,registry,expected",
         [
-            ("8.2-ubi9", "registry.redhat.io/ubi9/php-82:latest"),
-            ("8.2-ubi8", "registry.redhat.io/ubi8/php-82:latest"),
-            ("8.1-ubi9", "registry.redhat.io/ubi9/php-81:latest"),
-            ("8.0-ubi9", "registry.redhat.io/ubi9/php-80:latest"),
-            ("8.0-ubi8", "registry.redhat.io/ubi8/php-80:latest"),
-            ("7.4-ubi8", "registry.redhat.io/ubi8/php-74:latest"),
+            ("8.2-ubi9", "registry.redhat.io/ubi9/php-82:latest", True),
+            ("8.2-ubi8", "registry.redhat.io/ubi8/php-82:latest", True),
+            ("8.1-ubi9", "registry.redhat.io/ubi9/php-81:latest", True),
+            ("8.0-ubi9", "registry.redhat.io/ubi9/php-80:latest", True),
+            ("8.0-ubi8", "registry.redhat.io/ubi8/php-80:latest", True),
+            ("7.4-ubi8", "registry.redhat.io/ubi8/php-74:latest", True),
         ],
     )
-    def test_package_imagestream(self, version, registry):
-        assert self.hc_api.helm_package()
-        assert self.hc_api.helm_installation()
-        assert self.hc_api.check_imagestreams(version=version, registry=registry)
+    def test_package_imagestream(self, version, registry, expected):
+        assert self.hc_api.check_imagestreams(version=version, registry=registry) == expected
 
 
 class TestHelmCentOSPHPImageStreams:
@@ -41,22 +41,22 @@ class TestHelmCentOSPHPImageStreams:
         package_name = "php-imagestreams"
         path = test_dir / "../charts/centos"
         self.hc_api = HelmChartsAPI(path=path, package_name=package_name, tarball_dir=test_dir)
+        assert self.hc_api.helm_package()
+        assert self.hc_api.helm_installation()
 
     def teardown_method(self):
         self.hc_api.delete_project()
 
     @pytest.mark.parametrize(
-        "version,registry",
+        "version,registry,expected",
         [
-            ("8.2-ubi9", "registry.access.redhat.com/ubi9/php-82:latest"),
-            ("8.2-ubi8", "registry.access.redhat.com/ubi8/php-82:latest"),
-            ("8.1-ubi9", "registry.access.redhat.com/ubi9/php-81:latest"),
-            ("8.0-ubi9", "registry.access.redhat.com/ubi9/php-80:latest"),
-            ("8.0-ubi8", "registry.access.redhat.com/ubi8/php-80:latest"),
-            ("7.4-ubi8", "registry.access.redhat.com/ubi8/php-74:latest"),
+            ("8.2-ubi9", "registry.access.redhat.com/ubi9/php-82:latest", True),
+            ("8.2-ubi8", "registry.access.redhat.com/ubi8/php-82:latest", True),
+            ("8.1-ubi9", "registry.access.redhat.com/ubi9/php-81:latest", True),
+            ("8.0-ubi9", "registry.access.redhat.com/ubi9/php-80:latest", True),
+            ("8.0-ubi8", "registry.access.redhat.com/ubi8/php-80:latest", True),
+            ("7.4-ubi8", "registry.access.redhat.com/ubi8/php-74:latest", True),
         ],
     )
-    def test_package_imagestream(self, version, registry):
-        assert self.hc_api.helm_package()
-        assert self.hc_api.helm_installation()
-        assert self.hc_api.check_imagestreams(version=version, registry=registry)
+    def test_package_imagestream(self, version, registry, expected):
+        assert self.hc_api.check_imagestreams(version=version, registry=registry) == expected
